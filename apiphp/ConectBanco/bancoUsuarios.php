d0daf0f9bc21a4d6776ab688d8b246392aa6f2be
Rhuan Guilherme <rhuan23032004@gmail.com>
2023-10-20 17:58:22 -0300

    :art: Ajustes na conexão com banco de dados

--- apiphp/ConectBanco/bancoUsuarios.php
+++ apiphp/ConectBanco/bancoUsuarios.php
@@ -1,9 +1,9 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbName = "login";
+$username = "u417659758_rhuan23g";
+$password = "8y2+7*XP!b";
+$dbName = "u417659758_n1track";
 
 $conexao = new mysqli($servername, $username, $password, $dbName);
 
