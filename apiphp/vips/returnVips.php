ecca79a8ab1066ebd9d2443af4875b7ac41e7512
Rhuan Guilherme de Sousa Silva <rhuan.g.silva@stf.jus.br>
2024-01-17 09:10:13 -0300

    Vips

---
+++ apiphp/vips/returnVips.php
@@ -0,0 +1,30 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET");
+header("Content-Type: application/json");
+
+require '../ConectBanco/bancoUsuarios.php';
+
+if ($conexao->connect_error) {
+  die("Conexão falhou: " . $conexao->connect_error);
+}
+
+$sql = "SELECT * FROM vips ORDER BY id DESC";
+
+$result = $conexao->query($sql);
+
+if ($result === FALSE) {
+  die("Erro na consulta: " . $conexao->error);
+}
+
+$dados = array();
+
+while ($row = $result->fetch_assoc()) {
+  $dados[] = $row;
+}
+
+// Retorna os dados em formato JSON
+echo json_encode($dados);
+
+$conexao->close();
+?>
